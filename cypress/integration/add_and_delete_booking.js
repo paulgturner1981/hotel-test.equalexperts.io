ff8e5df9a1692703dd55418ef5c9b7080f85c173
Paul Turner <paulturner1981@gmail.com>
2019-11-28 16:27:22 +0000

    changed tests

--- cypress/integration/add_and_delete_booking.js
+++ cypress/integration/add_and_delete_booking.js
@@ -17,10 +17,21 @@ describe('Add a booking', function() {
     	.click();
     cy.get('.ui-datepicker-days-cell-over > .ui-state-default')
     	.click();
-    cy.get('#form > .row > :nth-child(7) > input')
+    cy.get('#form > .row input').last()
     	.click();
     cy.contains('TEST000001');
     cy.contains('BOOKING00001');
     cy.contains('99.99');
      })
+})
+
+describe('Deletes the booking', function() {
+  it('Deletes a hotel booking', function() {
+    cy.visit('/');
+    cy.get('#bookings > .row input[value*="Delete"]').last()
+    	.click();
+	cy.contains('TEST000001').should('not.exist');
+    cy.contains('BOOKING00001').should('not.exist');
+    cy.contains('99.99').should('not.exist');;        
+         })
 })
