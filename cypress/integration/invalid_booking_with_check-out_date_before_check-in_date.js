478f5aee710f53ae4fef0799ac15fba0cb5b3b08
paulgturner1981 <paulturner1981@gmail.com>
2019-12-11 21:37:23 +0000

    Updated tests
    Signed-off-by: paulgturner1981 <paulturner1981@gmail.com>

--- cypress/integration/invalid_booking_with_check-out_date_before_check-in_date.js
+++ cypress/integration/invalid_booking_with_check-out_date_before_check-in_date.js
@@ -2,28 +2,29 @@ describe('Attempt to make a booking with invalid dates', function() {
   it('Attempts to make a booking with a check-out date before the check-in date', function() {
     cy.visit('/');
     cy.get('#form #firstname')
-    	.type('INVALID12345');
+    	.type('CHECKOUT_DATE'); //Adds a first name for the booking
     cy.get('#form #lastname')
-    	.type('BOOKING12345');
+    	.type('BEFORE_CHECKIN_DATE'); //Adds a last name for the booking
     cy.get('#form #totalprice')
-    	.type('999.99');
+    	.type('777'); //Adds a price for the booking
     cy.get('#form #depositpaid')
-    	.select('false');
+    	.select('false'); //Selects the option for no deposit
     cy.get('#form #checkin')
     	.click();
     cy.get('.ui-datepicker-days-cell-over > .ui-state-default')
     cy.get(':nth-child(2) > :nth-child(3) > .ui-state-default')
-    	.click();
+    	.click(); //Opens the check-in datepicker and picks a date
     cy.get('#form #checkout')
     	.click();
     cy.get('.ui-datepicker-days-cell-over > .ui-state-default')
     cy.get(':nth-child(1) > :nth-child(1) > .ui-state-default')
-    	.click();
+    	.click(); //Opens the check-out datepicker and picks a date prior to the check-in date (invalid)
     cy.get('#form > .row input').last()
     	.click();
-    cy.contains('INVALID12345').should('not.exist');;
-    cy.contains('BOOKING12345').should('not.exist');;
-    cy.contains('999.99').should('not.exist');;
+    cy.wait(4000)
+    cy.contains('CHECKOUT_DATE').should('not.exist');;
+    cy.contains('BEFORE_CHECKIN_DATE').should('not.exist');;
+    cy.contains('777').should('not.exist');;
      })
 })
 
---
+++ cypress/integration/invalid_booking_with_date_in_the_past.js
@@ -0,0 +1,28 @@
+describe('Attempt to make a booking with invalid dates', function() {
+  it('Attempts to make a booking with dates in the past', function() {
+    cy.visit('/');
+    cy.get('#form #firstname')
+    	.type('DATE'); //Adds a first name for the booking
+    cy.get('#form #lastname')
+    	.type('IN_THE_PAST'); //Adds a last name for the booking
+    cy.get('#form #totalprice')
+    	.type('999.99'); //Adds a price for the booking
+    cy.get('#form #depositpaid')
+    	.select('false'); //Selects the option for no deposit
+      cy.get('#form #checkin').click();
+      cy.get('.ui-datepicker-days-cell-over > .ui-state-default');
+    cy.get('.ui-datepicker-prev > .ui-icon').click();
+    cy.get(':nth-child(2) > :nth-child(3) > .ui-state-default').click(); //Opens the check-in datepicker and picks the 10th of the previous month
+    cy.get('#form #checkout').click();
+    cy.get('.ui-datepicker-days-cell-over > .ui-state-default');
+    cy.get(':nth-child(1) > :nth-child(1) > .ui-state-default').click(); //Opens the check-out datepicker and picks the 10th of the previous month
+    cy.get('#form > .row input').last().click(); // Clicks the Save button
+      cy.wait(4000)
+      cy.contains('DATE').should('not.exist');
+    cy.contains('IN_THE_PAST').should('not.exist');
+    cy.contains('999.99').should('not.exist');
+     })
+});
+
+// NOTE: At present this test fails. This is correct. This is a defect (Defect ID: #2 in the defect report).
+// Once this defect has been fixed this test should then pass.
